8c0186dc17a13c3a7a14429703550e2f310e432c
Prerit Garg <prerit2010@gmail.com>
2016-07-05 17:19:00 +0530

    Separated CHECKS list and calling functions to test the requirements. swc-main.py is now the main file, which need to be run. It imports requirements_check.py and api.py for checking and pushing the data to server

--- requirements_check.py
+++ requirements_check.py
@@ -80,48 +80,6 @@ if not hasattr(_shlex, 'quote'):  # Python versions older than 3.3
 
 __version__ = '0.1'
 # HOST = "127.0.0.1:5000"
-HOST = "installation.software-carpentry.org"
-
-# Comment out any entries you don't need
-CHECKS = [
-# Shell
-    'virtual-shell',
-# Editors
-    'virtual-editor',
-# Browsers
-    'virtual-browser',
-# Version control
-    'git',
-    'hg',              # Command line tool
-    #'mercurial',       # Python package
-    'EasyMercurial',
-# Build tools and packaging
-    'make',
-    'virtual-pypi-installer',
-    'setuptools',
-    #'xcode',
-# Testing
-    'nosetests',       # Command line tool
-    'nose',            # Python package
-    'py.test',         # Command line tool
-    'pytest',          # Python package
-# SQL
-    'sqlite3',         # Command line tool
-    'sqlite3-python',  # Python package
-# Python
-    'python',
-    'ipython',         # Command line tool
-    'IPython',         # Python package
-    'argparse',        # Useful for utility scripts
-    'numpy',
-    'scipy',
-    'matplotlib',
-    'pandas',
-    # 'sympy',
-    # 'Cython',
-    # 'networkx',
-    # 'mayavi.mlab',
-    ]
 
 CHECKER = {}
 
@@ -257,8 +215,8 @@ def check(checks=None):
     successes = []
     failures = []
     failure_messages = []
-    if not checks:
-        checks = CHECKS
+    # if not checks:
+    #     checks = CHECKS
     for check in checks:
         try:
             checker = CHECKER[check]
@@ -1022,48 +980,3 @@ def print_suggestions(instructor_fallback=True):
         print('')
         print('For help, email the *entire* output of this script to')
         print('your instructor.')
-
-
-if __name__ == '__main__':
-    import optparse as _optparse
-    import api as API
-
-    parser = _optparse.OptionParser(usage='%prog [options] [check...]')
-    epilog = __doc__
-    parser.format_epilog = lambda formatter: '\n' + epilog
-    parser.add_option(
-        '-v', '--verbose', action='store_true',
-        help=('print additional information to help troubleshoot '
-              'installation issues'))
-    parser.add_option(
-        '-H', '--host', action='store', type="string",
-        help=('Change the server to which the data will be sent'), dest="host_name")
-    parser.add_option(
-        '-n', '--no_reporting', action='store_true',
-        help=('Turn off sending the data to server'))
-    options,args = parser.parse_args()
-    try:
-        passed, successes_list, failures_list = check(args)
-        """Check whether host name is specified as a command line argument"""
-        if options.host_name:
-            HOST = options.host_name
-        """Check whether sending data to server is turned off using
-           command line argument"""
-        if options.no_reporting is None:
-            API.submit(successes_list, failures_list, HOST)
-    except InvalidCheck as e:
-        print("I don't know how to check for {0!r}".format(e.check))
-        print('I do know how to check for:')
-        for key,checker in sorted(CHECKER.items()):
-            if checker.long_name != checker.name:
-                print('  {0} {1}({2})'.format(
-                        key, ' '*(20-len(key)), checker.long_name))
-            else:
-                print('  {0}'.format(key))
-        _sys.exit(1)
-    if not passed:
-        if options.verbose:
-            print()
-            print_system_info()
-            print_suggestions(instructor_fallback=True)
-        _sys.exit(1)
---
+++ swc-main.py
@@ -0,0 +1,92 @@
+import requirements_check
+from requirements_check import InvalidCheck, CHECKER
+import api as API
+import optparse as _optparse
+import sys as _sys
+
+HOST = "installation.software-carpentry.org"
+
+# Comment out any entries you don't need
+CHECKS = [
+# Shell
+    'virtual-shell',
+# Editors
+    'virtual-editor',
+# Browsers
+    'virtual-browser',
+# Version control
+    'git',
+    'hg',              # Command line tool
+    #'mercurial',       # Python package
+    'EasyMercurial',
+# Build tools and packaging
+    'make',
+    'virtual-pypi-installer',
+    'setuptools',
+    #'xcode',
+# Testing
+    'nosetests',       # Command line tool
+    'nose',            # Python package
+    'py.test',         # Command line tool
+    'pytest',          # Python package
+# SQL
+    'sqlite3',         # Command line tool
+    'sqlite3-python',  # Python package
+# Python
+    'python',
+    'ipython',         # Command line tool
+    'IPython',         # Python package
+    'argparse',        # Useful for utility scripts
+    'numpy',
+    'scipy',
+    'matplotlib',
+    'pandas',
+    # 'sympy',
+    # 'Cython',
+    # 'networkx',
+    # 'mayavi.mlab',
+    ]
+
+if __name__ == '__main__':
+
+    parser = _optparse.OptionParser(usage='%prog [options] [check...]')
+    epilog = __doc__
+    parser.format_epilog = lambda formatter: '\n' + epilog
+    parser.add_option(
+        '-v', '--verbose', action='store_true',
+        help=('print additional information to help troubleshoot '
+              'installation issues'))
+    parser.add_option(
+        '-H', '--host', action='store', type="string",
+        help=('Change the server to which the data will be sent'), dest="host_name")
+    parser.add_option(
+        '-n', '--no_reporting', action='store_true',
+        help=('Turn off sending the data to server'))
+    options,args = parser.parse_args()
+    try:
+        if not args:
+            args = CHECKS
+        passed, successes_list, failures_list = requirements_check.check(args)
+        """Check whether host name is specified as a command line argument"""
+        if options.host_name:
+            HOST = options.host_name
+        """Check whether sending data to server is turned off using
+           command line argument"""
+        if options.no_reporting is None:
+            API.submit(successes_list, failures_list, HOST)
+    except InvalidCheck as e:
+        print("I don't know how to check for {0!r}".format(e.check))
+        print('I do know how to check for:')
+        for key,checker in sorted(CHECKER.items()):
+            if checker.long_name != checker.name:
+                print('  {0} {1}({2})'.format(
+                        key, ' '*(20-len(key)), checker.long_name))
+            else:
+                print('  {0}'.format(key))
+        _sys.exit(1)
+    if not passed:
+        if options.verbose:
+            print()
+            requirements_check.print_system_info()
+            requirements_check.print_suggestions(instructor_fallback=True)
+        _sys.exit(1)
